f394ba8df1ca6eb73a93a08da20ddb54032a5d5e
Darth Vader <danieltarancon@gmail.com>
2022-11-06 00:01:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,42 @@ Teams = TEAMS
 
 Players = PLAYERS
 
-def clean_data():
-    for player in Players:
+def clean_data(data):
+    for player in data:
         for key, value in player.items():
             if key == 'experience':
-                bool(value)
-            elif key == 'guardians':
+                player[key] = bool(value)
+        for key, value in player.items():
+            if key == 'guardians':
                 if 'and' in value:
-                    value.split('and')
+                    player[key] = value.split('and')
                 else:
-                    value.split()
+                    player[key] = []
+                    player[key].append(value)
+    return data
+
+
+c_player = clean_data(Players)
+
+for i, v  in c_player[0].items():
+    print(type(v))
+
+
+print(c_player)
+
+
+
+
+
+def balance_teams(Teams):
+    for team in Teams:
+        team = []
+        pass
+
+
+
+
+
 
 
 

60734729c981975996080e34a34b560ed7b2ff78
Darth Vader <danieltarancon@gmail.com>
2022-11-05 20:05:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from constants import TEAMS, PLAYERS
 
+Teams = TEAMS
+
+Players = PLAYERS
 
 def clean_data():
-    for player in PLAYERS:
-        for 
+    for player in Players:
+        for key, value in player.items():
+            if key == 'experience':
+                bool(value)
+            elif key == 'guardians':
+                if 'and' in value:
+                    value.split('and')
+                else:
+                    value.split()
+
+
+
 
 
 

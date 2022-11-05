66ef89c14130d21f954b2fab5c4cf3685bbcc292
Darth Vader <danieltarancon@gmail.com>
2022-11-06 00:26:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import random
+
 from constants import TEAMS, PLAYERS
 
 Teams = TEAMS
@@ -19,22 +21,39 @@ def clean_data(data):
     return data
 
 
-c_player = clean_data(Players)
-
-for i, v  in c_player[0].items():
-    print(type(v))
-
-
-print(c_player)
+c_players = clean_data(Players)
 
 
 
 
+print(c_players)
 
-def balance_teams(Teams):
+n_players = len(Players) / len(Teams)
+
+
+
+def balance_teams(Teams, c_players):
+    Final_Teams = []
     for team in Teams:
         team = []
-        pass
+        while len(team) < n_players:
+            n = random.Random(1, len(c_players) + 1)
+            team.append(c_players[n])
+            c_players.remove[n]
+        Final_Teams.append(team)
+    return Final_Teams
+
+
+Final_Teams = balance_teams(Teams, c_players)
+
+print(Final_Teams)
+
+
+
+
+
+
+
 
 
 

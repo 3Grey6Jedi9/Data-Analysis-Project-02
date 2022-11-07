dd475cfb8706821d73b24435fcccd566139be0b5
Darth Vader <danieltarancon@gmail.com>
2022-11-07 21:14:52 +0100

    Update prk.py

--- prk.py
+++ prk.py
@@ -2,17 +2,14 @@
 
 
 
-d = {
-        'name': 'Karl Saygan',
-        'guardians': 'Heather Bledsoe',
-        'experience': 'YES',
-        'height': '42 inches'
-    }
+d = ['Anna', 'Peter']
 
 
-for key, value in d.items():
-    if key == 'height':
-        d[key] = int((value.split(' ')[0]))
-        print(type(d[key]))
+e = set(d)
+
+print(e)
+
+for name in e:
+    print(name, end=', ')
 
 
